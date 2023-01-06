01f9861082690e9d14a68df1cf678aba1045d550
natadekoko <risobirin98@gmail.com>
2023-01-06 23:36:27 +0700

    day 5: intro MVC .Net

---
+++ WebMVC/WebMVC/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebMVC.Models;
+
+namespace WebMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult ContactUS()
+    {
+        return View();
+    }
+
+    public IActionResult AboutUS()
+    {
+        return View();
+    }
+    
+    public IActionResult Admin()
+    {
+        return View();
+    }
+    
+    public IActionResult Request()
+    {
+        return View();
+    }
+    public IActionResult Talent()
+    {
+        return View();
+    }
+    public IActionResult Asset()
+    {
+        return View();
+    }
+    public IActionResult Audit()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
