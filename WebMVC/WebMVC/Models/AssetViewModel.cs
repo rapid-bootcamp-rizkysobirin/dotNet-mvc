0f73b8bded3b46f96323fb81543d8e0f55856665
natadekoko <risobirin98@gmail.com>
2023-01-08 10:03:11 +0700

    CRUD Asset solve

--- WebMVC/WebMVC/Models/AssetViewModel.cs
+++ WebMVC/WebMVC/Models/AssetViewModel.cs
@@ -1,23 +1,25 @@
-namespace WebMVC.Models;
-
-public class AssetViewModel
+namespace WebMVC.Models
 {
-    public int Id { get; set; }
-    public String Name { get; set; }
-    public String Specification { get; set; }
-    public String SerialNumber { get; set; }
-    public int PurchaseYear { get; set; }
 
-    public AssetViewModel()
+    public class AssetViewModel
     {
-    }
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Specification { get; set; }
+        public String SerialNumber { get; set; }
+        public int PurchaseYear { get; set; }
 
-    public AssetViewModel(int id, string name, string specification, string serialNumber, int purchaseYear)
-    {
-        Id = id;
-        Name = name;
-        Specification = specification;
-        SerialNumber = serialNumber;
-        PurchaseYear = purchaseYear;
+        public AssetViewModel()
+        {
+        }
+
+        public AssetViewModel(int id, string name, string specification, string serialNumber, int purchaseYear)
+        {
+            Id = id;
+            Name = name;
+            Specification = specification;
+            SerialNumber = serialNumber;
+            PurchaseYear = purchaseYear;
+        }
     }
 }
